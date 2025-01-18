eab064d73584f37768b39e109d8b7a89d4d6e4ee
BlackCoffeee <arief.veriez@gmail.com>
2025-01-18 21:54:02 +0800

    Add main application router component with initial routes

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,24 @@
+/**
+ * Komponen Router utama aplikasi
+ * @author Muhammad Arif <https://github.com/BlackCoffeee>
+ * @createAt 2025-01-18
+ */
+
+import { Routes, Route } from 'react-router-dom'
+import { RootLayout } from '@/components/layouts/root-layout'
+import { Dashboard } from '@/pages'
+
+export function AppRouter() {
+    return (
+        <Routes>
+            <Route element={<RootLayout />}>
+                <Route path='/' element={<Dashboard />} />
+                <Route path='/ecommerce' element={<div>Ecommerce Page</div>} />
+                <Route path='/project' element={<div>Project Page</div>} />
+                <Route path='/marketing' element={<div>Marketing Page</div>} />
+                <Route path='/analytics' element={<div>Analytics Page</div>} />
+                {/* Tambahkan route lainnya sesuai kebutuhan */}
+            </Route>
+        </Routes>
+    )
+}
