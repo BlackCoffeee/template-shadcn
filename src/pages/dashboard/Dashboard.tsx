7fe7edea01bf667fbea273af594e2d3d5abcaed6
BlackCoffeee <arief.veriez@gmail.com>
2025-01-18 21:55:10 +0800

    Add initial pages: Dashboard and index export

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,197 @@
+/**
+ * Halaman Dashboard
+ * @author Muhammad Arif <https://github.com/BlackCoffeee>
+ * @createAt 2025-01-18
+ */
+import React from 'react'
+
+const Dashboard: React.FC = () => {
+    return (
+        <div className='flex flex-col gap-4'>
+            <h1 className='text-3xl font-bold'>Dashboard</h1>
+            <p className='text-muted-foreground'>
+                Selamat datang di dashboard aplikasi Anda
+            </p>
+
+            <div className='mt-4'>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                    Curabitur pretium tincidunt lacus. Nulla gravida orci a
+                    odio, et viverra elit sagittis. Fusce a quam. Etiam ut
+                    tellus eu augue luctus tincidunt.
+                </p>
+                <p>
+                    Morbi lectus risus, iaculis vel, suscipit quis, luctus non,
+                    massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris
+                    ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed,
+                    euismod in, nibh.
+                </p>
+                <p>
+                    Quisque volutpat condimentum velit. Class aptent taciti
+                    sociosqu ad litora torquent per conubia nostra, per inceptos
+                    himenaeos. Morbi lectus risus, iaculis vel, suscipit quis,
+                    luctus non, massa.
+                </p>
+                <p>
+                    Donec mi odio, faucibus at, scelerisque quis, convallis in,
+                    nisi. Morbi lectus risus, iaculis vel, suscipit quis, luctus
+                    non, massa. Fusce ac turpis quis ligula lacinia aliquet.
+                </p>
+                <p>
+                    Nulla metus metus, ullamcorper vel, tincidunt sed, euismod
+                    in, nibh. Quisque volutpat condimentum velit. Class aptent
+                    taciti sociosqu ad litora torquent per conubia nostra, per
+                    inceptos himenaeos.
+                </p>
+                <p>
+                    Morbi lectus risus, iaculis vel, suscipit quis, luctus non,
+                    massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris
+                    ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed,
+                    euismod in, nibh.
+                </p>
+                <p>
+                    Curabitur pretium tincidunt lacus. Nulla gravida orci a
+                    odio, et viverra elit sagittis. Fusce a quam. Etiam ut
+                    tellus eu augue luctus tincidunt.
+                </p>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </p>
+            </div>
+            <div className='mt-4'>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                    Curabitur pretium tincidunt lacus. Nulla gravida orci a
+                    odio, et viverra elit sagittis. Fusce a quam. Etiam ut
+                    tellus eu augue luctus tincidunt.
+                </p>
+                <p>
+                    Morbi lectus risus, iaculis vel, suscipit quis, luctus non,
+                    massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris
+                    ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed,
+                    euismod in, nibh.
+                </p>
+                <p>
+                    Quisque volutpat condimentum velit. Class aptent taciti
+                    sociosqu ad litora torquent per conubia nostra, per inceptos
+                    himenaeos. Morbi lectus risus, iaculis vel, suscipit quis,
+                    luctus non, massa.
+                </p>
+                <p>
+                    Donec mi odio, faucibus at, scelerisque quis, convallis in,
+                    nisi. Morbi lectus risus, iaculis vel, suscipit quis, luctus
+                    non, massa. Fusce ac turpis quis ligula lacinia aliquet.
+                </p>
+                <p>
+                    Nulla metus metus, ullamcorper vel, tincidunt sed, euismod
+                    in, nibh. Quisque volutpat condimentum velit. Class aptent
+                    taciti sociosqu ad litora torquent per conubia nostra, per
+                    inceptos himenaeos.
+                </p>
+                <p>
+                    Morbi lectus risus, iaculis vel, suscipit quis, luctus non,
+                    massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris
+                    ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed,
+                    euismod in, nibh.
+                </p>
+                <p>
+                    Curabitur pretium tincidunt lacus. Nulla gravida orci a
+                    odio, et viverra elit sagittis. Fusce a quam. Etiam ut
+                    tellus eu augue luctus tincidunt.
+                </p>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </p>
+            </div>
+            <div className='mt-4'>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                    Curabitur pretium tincidunt lacus. Nulla gravida orci a
+                    odio, et viverra elit sagittis. Fusce a quam. Etiam ut
+                    tellus eu augue luctus tincidunt.
+                </p>
+                <p>
+                    Morbi lectus risus, iaculis vel, suscipit quis, luctus non,
+                    massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris
+                    ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed,
+                    euismod in, nibh.
+                </p>
+                <p>
+                    Quisque volutpat condimentum velit. Class aptent taciti
+                    sociosqu ad litora torquent per conubia nostra, per inceptos
+                    himenaeos. Morbi lectus risus, iaculis vel, suscipit quis,
+                    luctus non, massa.
+                </p>
+                <p>
+                    Donec mi odio, faucibus at, scelerisque quis, convallis in,
+                    nisi. Morbi lectus risus, iaculis vel, suscipit quis, luctus
+                    non, massa. Fusce ac turpis quis ligula lacinia aliquet.
+                </p>
+                <p>
+                    Nulla metus metus, ullamcorper vel, tincidunt sed, euismod
+                    in, nibh. Quisque volutpat condimentum velit. Class aptent
+                    taciti sociosqu ad litora torquent per conubia nostra, per
+                    inceptos himenaeos.
+                </p>
+                <p>
+                    Morbi lectus risus, iaculis vel, suscipit quis, luctus non,
+                    massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris
+                    ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed,
+                    euismod in, nibh.
+                </p>
+                <p>
+                    Curabitur pretium tincidunt lacus. Nulla gravida orci a
+                    odio, et viverra elit sagittis. Fusce a quam. Etiam ut
+                    tellus eu augue luctus tincidunt.
+                </p>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @author Muhammad Arif <https://github.com/BlackCoffeee>
+ * @createAt 2025-01-18
+ * @description File untuk mengekspor semua halaman
+ */
+
+export { default as Dashboard } from './dashboard/Dashboard'
+// export komponen halaman lainnya di sini
