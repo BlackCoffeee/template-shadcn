e36a551a50cd26c3157a4ce335af10cc415c26c3
BlackCoffeee <arief.veriez@gmail.com>
2025-01-19 15:51:06 +0800

    Update AppRouter to include registration and forgot password routes
    - Added a new route for the Register component at '/pages/auth/register', enhancing the authentication flow by allowing user registration.
    - Introduced a placeholder route for the Forgot Password page at '/pages/auth/forgot-password', improving user support for account recovery.
    - These changes streamline the navigation for authentication-related actions, providing a more comprehensive user experience.

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -6,7 +6,7 @@
 
 import { Routes, Route } from 'react-router-dom'
 import { RootLayout } from '@/components/layouts/root-layout'
-import { Dashboard, Login } from '@/pages'
+import { Dashboard, Login, Register } from '@/pages'
 
 export function AppRouter() {
     return (
@@ -20,6 +20,11 @@ export function AppRouter() {
                 {/* Tambahkan route lainnya sesuai kebutuhan */}
             </Route>
             <Route path='/pages/auth/login' element={<Login />} />
+            <Route
+                path='/pages/auth/forgot-password'
+                element={<div>Forgot Password Page</div>}
+            />
+            <Route path='/pages/auth/register' element={<Register />} />
         </Routes>
     )
 }
