f89d88d6c1bf755eb5930730789514c96a2bd7f4
BlackCoffeee <arief.veriez@gmail.com>
2025-01-19 12:06:24 +0800

    Add LayoutSettings component for menu layout configuration
    - Introduced a new LayoutSettings component to allow users to configure the menu layout as either horizontal or vertical.
    - Utilizes the useNavigationStore to manage the orientation state, enhancing user customization options.
    - This addition contributes to the ongoing efforts to improve the application's navigation experience and user interface.

---
+++ src/components/navigation/settings/layout-settings.tsx
@@ -0,0 +1,34 @@
+/**
+ * Komponen untuk mengatur tata letak menu
+ * @author Muhammad Arif <https://github.com/BlackCoffeee>
+ * @createAt 2024-01-19
+ */
+
+import { Button } from '@/components/ui/button'
+import { useNavigationStore } from '@/store/navigation-store'
+
+export function LayoutSettings() {
+    const setOrientation = useNavigationStore(state => state.setOrientation)
+
+    return (
+        <div className='space-y-2'>
+            <h4 className='text-sm font-medium'>Tata Letak Menu</h4>
+            <div className='flex flex-col gap-2'>
+                <Button
+                    variant='outline'
+                    className='justify-start'
+                    onClick={() => setOrientation('horizontal')}
+                >
+                    Menu Horizontal
+                </Button>
+                <Button
+                    variant='outline'
+                    className='justify-start'
+                    onClick={() => setOrientation('vertical')}
+                >
+                    Menu Vertikal
+                </Button>
+            </div>
+        </div>
+    )
+}
