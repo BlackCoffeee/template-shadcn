82bd0f10aeecae575cf15bfe4d7a1f28fcc9da7b
BlackCoffeee <arief.veriez@gmail.com>
2025-01-19 12:04:41 +0800

    Refactor App component to integrate ThemeColorProvider for enhanced theming
    - Wrapped the AppRouter with the new ThemeColorProvider to manage theme colors dynamically.
    - This change improves the overall theme management and aligns with ongoing efforts to enhance the application's visual consistency and user experience.

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,18 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './components/providers/theme-provider'
 import { TooltipProvider } from './components/providers/tooltip-provider'
+import { ThemeColorProvider } from './components/providers/theme-color-provider'
 import { AppRouter } from './router/AppRouter'
 
 function App() {
     return (
         <BrowserRouter>
             <ThemeProvider>
-                <TooltipProvider>
-                    <AppRouter />
-                </TooltipProvider>
+                <ThemeColorProvider>
+                    <TooltipProvider>
+                        <AppRouter />
+                    </TooltipProvider>
+                </ThemeColorProvider>
             </ThemeProvider>
         </BrowserRouter>
     )
