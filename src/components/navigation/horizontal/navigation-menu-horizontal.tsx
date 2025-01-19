4a5547d9e8599cd15ece49802bb59f5aa1c5b994
BlackCoffeee <arief.veriez@gmail.com>
2025-01-19 20:59:29 +0800

    Enhance Navigation Menu with Logo Integration
    - Added a Logo component to the NavigationMenuHorizontal for improved branding visibility.
    - Adjusted the layout by increasing the gap between items, enhancing the overall navigation structure and user experience.

--- src/components/navigation/horizontal/navigation-menu-horizontal.tsx
+++ src/components/navigation/horizontal/navigation-menu-horizontal.tsx
@@ -9,11 +9,13 @@ import { menuItems } from '../menu-items'
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
 import { NavigationMenuItem } from '../items/navigation-menu-item'
 import { NavigationMenuItemHorizontalWithChildren } from '../items/navigation-menu-item-horizontal-with-children'
+import { Logo } from '@/components/brand/logo'
 
 export function NavigationMenuHorizontal() {
     return (
         <ScrollArea className='max-w-[600px] lg:max-w-none'>
-            <nav className={cn('flex items-center gap-1 px-2')}>
+            <nav className={cn('flex items-center gap-4 px-2')}>
+                <Logo className='mr-4' />
                 {menuItems.map(item =>
                     item.children ? (
                         <NavigationMenuItemHorizontalWithChildren
