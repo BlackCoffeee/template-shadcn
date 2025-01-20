5301a69452cc00bec6df13052fd0f2d6869fecc0
BlackCoffeee <arief.veriez@gmail.com>
2025-01-20 14:11:23 +0800

    Add Profile component export to index.ts for account management

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -8,4 +8,5 @@ export { default as Dashboard } from './dashboard/Dashboard'
 export { default as Login } from './authentication/Login'
 export { default as Register } from './authentication/Register'
 export { default as ForgotPassword } from './authentication/ForgotPassword'
+export { default as Profile } from './account/Profile'
 // export komponen halaman lainnya di sini
