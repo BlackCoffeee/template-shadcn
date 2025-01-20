25196e45dc284dc8051bc84ab3ae139031a6227b
BlackCoffeee <arief.veriez@gmail.com>
2025-01-20 18:35:00 +0800

    Add ProfileTabs component for navigation in user profile

---
+++ src/components/profile/profile-tabs.tsx
@@ -0,0 +1,26 @@
+/**
+ * Komponen ProfileTabs untuk menampilkan tab navigasi pada halaman profile
+ * @author Muhammad Arif <https://github.com/BlackCoffeee>
+ * @createAt 2024-03-20
+ */
+
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs'
+import { ProfileTimeline } from './profile-timeline'
+import { ProfileAbout } from './profile-about'
+
+export function ProfileTabs() {
+    return (
+        <Tabs defaultValue='about' className='w-full'>
+            <TabsList className='grid w-full grid-cols-2'>
+                <TabsTrigger value='about'>About</TabsTrigger>
+                <TabsTrigger value='timeline'>Timeline</TabsTrigger>
+            </TabsList>
+            <TabsContent value='about'>
+                <ProfileAbout />
+            </TabsContent>
+            <TabsContent value='timeline'>
+                <ProfileTimeline />
+            </TabsContent>
+        </Tabs>
+    )
+}
